4c624ef188d00e3a875e1c515abfeee510111aa4
glopez <george.ramirez.lopez@gmail.com>
2017-04-25 18:21:17 -0700

    Add project files.

---
+++ ErrorEventLogger/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorEventLogger
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
