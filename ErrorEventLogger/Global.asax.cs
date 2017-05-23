7cd93cf1d39d989afba9a7e57f2955b4c6f97086
glopez <george.ramirez.lopez@gmail.com>
2017-05-22 23:01:07 -0700

    Created ErrorLogging Class Library

--- ErrorEventLogger/Global.asax.cs
+++ ErrorEventLogger/Global.asax.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using log4net;
+using ErrorLogging;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -19,6 +20,9 @@ namespace ErrorEventLogger
         private static readonly ILog log =
             log4net.LogManager.GetLogger(System.Reflection.MethodBase
                 .GetCurrentMethod().DeclaringType);
+
+        //private static readonly ILog log = LogHelper.GetLogger(System.Reflection.MethodBase
+        //        .GetCurrentMethod().DeclaringType);
 
         protected void Application_Start()
         {
@@ -32,26 +36,26 @@ namespace ErrorEventLogger
         {
             Exception ex = Server.GetLastError();
 
-            if(ex != null)
+            if (ex != null)
             {
-                StringBuilder errorString = new StringBuilder();
-                errorString.Append("An error was caught in the Application_Error event.\n");
-                errorString.Append("Error in: " + (Context.Session == null ? string.Empty : Request.Url.ToString()));
-                errorString.Append("\nError Message: " + ex.Message);
+                string sessionState;
 
-                if (ex.InnerException != null)
-                    errorString.Append("\nInner Error Message: " + ex.InnerException.Message);
+                if (Context.Session == null)
+                    sessionState = string.Empty;
 
-                errorString.Append("\n\nStack Trace: " + ex.StackTrace);
+                else
+                    sessionState = Request.Url.ToString();
+
+
+                string currentThread = Thread.CurrentPrincipal.Identity.Name;
+
+                bool authenticated = Thread.CurrentPrincipal.Identity.IsAuthenticated;
+
+                string errorString = LogHelper.CreateWebLog(sessionState, currentThread, authenticated, ex);
 
                 Server.ClearError();
 
-                if(Context.Session != null)
-                {
-                    errorString.Append($"Session: Identity name:[{Thread.CurrentPrincipal.Identity.Name}] IsAuthenticated:{Thread.CurrentPrincipal.Identity.IsAuthenticated}");
-                }
-                
-                log.Error(errorString.ToString());
+                log.Error(errorString);
 
                 if (Context.Session != null)
                 {
@@ -60,7 +64,7 @@ namespace ErrorEventLogger
                     routeData.Values.Add("action", "Error");
                     routeData.Values.Add("exception", ex);
 
-                    if(ex.GetType() == typeof(HttpException))
+                    if (ex.GetType() == typeof(HttpException))
                     {
                         routeData.Values.Add("statusCode", ((HttpException)ex).GetHttpCode());
                     }
@@ -74,6 +78,49 @@ namespace ErrorEventLogger
                     controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
                     Response.End();
                 }
+
+            //if (ex != null)
+            //{
+            //    StringBuilder errorString = new StringBuilder();
+            //    errorString.Append("An error was caught in the Application_Error event.\n");
+            //    errorString.Append("Error in: " + (Context.Session == null ? string.Empty : Request.Url.ToString()));
+            //    errorString.Append("\nError Message: " + ex.Message);
+
+            //    if (ex.InnerException != null)
+            //        errorString.Append("\nInner Error Message: " + ex.InnerException.Message);
+
+            //    errorString.Append("\n\nStack Trace: " + ex.StackTrace);
+
+            //    Server.ClearError();
+
+            //    if (Context.Session != null)
+            //    {
+            //        errorString.Append($"Session: Identity name:[{Thread.CurrentPrincipal.Identity.Name}] IsAuthenticated:{Thread.CurrentPrincipal.Identity.IsAuthenticated}");
+            //    }
+
+            //    log.Error(errorString.ToString());
+
+            //    if (Context.Session != null)
+            //    {
+            //        var routeData = new RouteData();
+            //        routeData.Values.Add("controller", "Error");
+            //        routeData.Values.Add("action", "Error");
+            //        routeData.Values.Add("exception", ex);
+
+            //        if (ex.GetType() == typeof(HttpException))
+            //        {
+            //            routeData.Values.Add("statusCode", ((HttpException)ex).GetHttpCode());
+            //        }
+            //        else
+            //        {
+            //            routeData.Values.Add("statusCode", 500);
+            //        }
+
+            //        Response.TrySkipIisCustomErrors = true;
+            //        IController controller = new ErrorController();
+            //        controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            //        Response.End();
+            //    }
             }
         }
     }
---
+++ ErrorLogging/LogHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Appender;
+
+namespace ErrorLogging
+{
+    public class LogHelper
+    {
+        public static ILog GetLogger(Type type)
+        {
+            ILog log = log4net.LogManager.GetLogger(type);
+            //FileAppender _fileAppender = new FileAppender()
+
+            return (log);
+        }
+
+        public static string CreateWebLog(string sessionState, string currentThread, bool authenticated, Exception ex)
+        {
+            StringBuilder errorString = new StringBuilder();
+
+            if (ex != null)
+            {
+                //StringBuilder errorString = new StringBuilder();
+                errorString.Append("An error was caught in the Application_Error event.\n");
+                errorString.Append("Error in: " + (sessionState));
+                errorString.Append("\nError Message: " + ex.Message);
+
+                if (ex.InnerException != null)
+                    errorString.Append("\nInner Error Message: " + ex.InnerException.Message);
+
+                errorString.Append("\n\nStack Trace: " + ex.StackTrace);
+
+                if (sessionState != null)
+                {
+                    errorString.Append($"Session: Identity name:[{currentThread}] IsAuthenticated:{authenticated}");
+                }
+
+                //log.Error(errorString.ToString());
+            }
+
+            return errorString.ToString();      
+        }
+    }
+}
